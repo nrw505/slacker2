ba5b74cfc1eb94e8d2098783d89ebd33c5934fb6
Nigel Williams <nigel.williams@marketplacer.com>
2023-01-17 11:19:53 +1100

    chore: handle modal submission

--- slacker/bot/review_listener.py
+++ slacker/bot/review_listener.py
@@ -4,6 +4,10 @@ from slack_sdk.socket_mode.request import SocketModeRequest
 from slack_sdk.socket_mode.listeners import SocketModeRequestListener
 
 
+def verify_github_pr_url(url):
+    return False
+
+
 def review_listener(client: BaseSocketModeClient, request: SocketModeRequest) -> None:
     # Is it for me?
     if (
@@ -11,6 +15,10 @@ def review_listener(client: BaseSocketModeClient, request: SocketModeRequest) ->
         and request.payload.get("type") == "shortcut"
         and request.payload["callback_id"] == "review"
     ):
+        # Acknowledge the event
+        response = SocketModeResponse(envelope_id=request.envelope_id)
+        client.send_socket_mode_response(response)
+
         # respond to /slacker review
         client.web_client.views_open(
             trigger_id=request.payload["trigger_id"],
@@ -22,6 +30,7 @@ def review_listener(client: BaseSocketModeClient, request: SocketModeRequest) ->
                 "blocks": [
                     {
                         "type": "input",
+                        "block_id": "pr",
                         "label": {
                             "type": "plain_text",
                             "text": "PR's :github: GitHub URL",
@@ -42,5 +51,22 @@ def review_listener(client: BaseSocketModeClient, request: SocketModeRequest) ->
         and request.payload.get("type") == "view_submission"
         and request.payload["view"]["callback_id"] == "review-modal"
     ):
-        # Handle the review modal submission
-        pass
+        # Check the payload for errors
+        response_payload = None
+        pr_url = request.payload["view"]["state"]["values"]["pr"]["pr_url"]["value"]
+
+        print(f"PR URL: {pr_url}")
+        if not verify_github_pr_url(pr_url):
+            response_payload = {
+                "response_action": "errors",
+                "errors": {
+                    "pr": "That is not a GitHub PR URL",
+                },
+            }
+
+        # Send the acknowledgement, possibly with errors
+        print(f"response: {response_payload}")
+        response = SocketModeResponse(
+            envelope_id=request.envelope_id, payload=response_payload
+        )
+        client.send_socket_mode_response(response)
